1a8ee75d66aa106a871357d9da3bba766dd09a3c
jamie-ha-choi <jamie.ha.choi@gmail.com>
2026-08-12 17:03:12 +1200

    Added the c files for the questions done, renamed primecheck, and finished Q4.6

---
+++ Labs/Lab2_UART/Transmit_345.c
@@ -0,0 +1,52 @@
+/*
+ * GccApplication2.c
+ *
+ * Created: 10/08/2026 2:13:15 pm
+ * Author : disiz
+ */ 
+
+#include <avr/io.h>
+#include <stdint.h>
+#include <util/delay.h>
+
+#define F_CPU 2000000UL
+
+void usart_init(uint16_t ubrr)
+{
+    UBRR0H = (uint8_t)(ubrr >> 8);
+    UBRR0L = (uint8_t)ubrr;
+    UCSR0B = (1 << TXEN0);
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+void usart_transmit(uint8_t data)
+{
+	while (!(UCSR0A & (1 << UDRE0)))
+	{
+	}
+	UDR0 = data;
+}
+
+int main(void)
+{
+	uint16_t number = 345;
+	uint8_t hundreds;
+	uint8_t tens;
+	uint8_t ones;
+	
+	usart_init(12);
+
+	while(1)
+	{
+        hundreds = number / 100;
+        tens = (number / 10) % 10;
+        ones = number % 10;
+
+        usart_transmit(hundreds + 48);
+        usart_transmit(tens + 48);
+        usart_transmit(ones + 48);
+
+        _delay_ms(500);
+	}
+}
+
