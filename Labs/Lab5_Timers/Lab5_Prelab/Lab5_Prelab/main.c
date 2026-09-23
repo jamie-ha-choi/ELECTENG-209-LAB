0219e1a529647d566ff2ea40a388bbbddb8272f0
jamie-ha-choi <jamie.ha.choi@gmail.com>
2026-09-23 15:38:41 +1200

    FInished prelab

--- Labs/Lab5_Timers/Lab5_Prelab/Lab5_Prelab/main.c
+++ Labs/Lab5_Timers/Lab5_Prelab/Lab5_Prelab/main.c
@@ -5,14 +5,23 @@
  * Author : disiz
  */ 
 
-#include <avr/io.h>
+#define F_CPU 2000000UL
 
+#include <avr/io.h>
+#include <util/delay.h>
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
-}
+	DDRB = 0xFF;
+	DDRC = 0x00;
+	DDRD = 0x00;
 
+	while (1)
+	{
+		PORTB |= (1 << PB2);
+		_delay_ms(375);
+
+		PORTB &= ~(1 << PB2);
+		_delay_ms(125);
+	}
+}
