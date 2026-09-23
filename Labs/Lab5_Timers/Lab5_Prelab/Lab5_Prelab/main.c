c14102aa3be7de5bf55817938a0f9129aef11ba3
jamie-ha-choi <jamie.ha.choi@gmail.com>
2026-09-23 15:26:59 +1200

    Start prelab

---
+++ Labs/Lab5_Timers/Lab5_Prelab/Lab5_Prelab/main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab5_Prelab.c
+ *
+ * Created: 22/09/2026 2:02:05 pm
+ * Author : disiz
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
